b4b86c7f1eae3142cecdfe7412619218eb7f85fc
liyong <83774054+ly-rgb@users.noreply.github.com>
2023-09-17 20:02:37 +0800

    Create code.js

---
+++ AST/example/code.js
@@ -0,0 +1,25 @@
+//  还原 16进制字符串  和  unicode字符串 
+// function cV() {
+//   EV = L7V(cwV(PbllMMybxy), "PbllMMybxy", "\x35\x63\x66\x64\x35\x61\x35", "\u0066");
+// };
+
+
+//  还原  ASCII码混淆  和  eval加密
+//eval(String.fromCharCode(118, 97, 114, 32, 79, 79, 79, 79, 79, 48, 32, 61, 32, 79, 79, 79, 79, 79, 111, 59))
+
+
+// 还原  base64 编码
+// var arr = [1, 2, 3]
+// var xx = arr["bGVuZ3Ro"]
+
+
+//  还原 数值常量加密   位异或运算
+// eval(String.fromCharCode(425871 ^ 425977, 118436 ^ 118469));
+
+
+//  还原 逗号表达式混淆
+// function test2(a, b, c, d, e, f){
+//   return f = (e = ( d = (c = (b = (a = 1000, a + 50, b + 60, c + 70, a + 2000),
+//   d + 80, b + 3000), e + 90, c + 4000), f + 100, d + 5000), e + 6000)
+// };
+
