5e86d0883b204ed6399d208a9f1c82c145253159
liyong <83774054+ly-rgb@users.noreply.github.com>
2023-09-20 09:32:38 +0800

    Create jsfuckConvert.js

---
+++ AST/example/jsfuckConvert.js
@@ -0,0 +1,33 @@
+//  还原  jsfuck编码
+
+const fs = require("fs");
+const types = require("@babel/types");
+const parser = require("@babel/parser");
+const traverse = require("@babel/traverse").default
+const generator = require("@babel/generator").default
+
+const code = fs.readFileSync("code.js", {encoding: "utf-8"});
+const ast = parser.parse(code, {sourceType: "module"});
+
+const visitor = {
+        "BinaryExpression|UnaryExpression"(path) {
+        if(path.isUnaryExpression({operator:"-"}) ||
+         path.isUnaryExpression({operator:"void"}))
+      {
+        return;
+      }
+        const {confident, value} = path.evaluate();
+        if (!confident)
+            return;
+        if (typeof value == 'number' && (!Number.isFinite(value))) {
+            return;
+        }
+        path.replaceWith(types.valueToNode(value));
+    }}
+
+traverse(ast, visitor);
+const newCode = generator(ast)
+fs.writeFile("newCode.js", newCode.code, (err)=>{})
+
+
+
